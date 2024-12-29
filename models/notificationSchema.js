9d856a17f1a80ea9fd034a0b1f83df8e3346d5fe
SB2318 <sb2318@it.jgec.ac.in>
2024-12-30 00:47:35 +0530

    --small-change

--- models/notificationSchema.js
+++ models/notificationSchema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const notifcationSchema = new mongoose.Schema({
-    userID:{
+    userId:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'User',
         required:true
