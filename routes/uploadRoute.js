0c28696056a269b4d91ad4de00f9b0468d12349c
SB2318 <sb2318@it.jgec.ac.in>
2025-01-11 10:41:51 +0530

    --server-up

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -15,8 +15,8 @@ module.exports = uploadRoute;
 // Upload Profile Image
 
 // Upload Article Image
-// Case I -> ImageUtils, will added at the time of post creation
-// Case II -> Article Image url will attached with content.
+// Case I -> ImageUtils, will added at the time of post creation 
+// Case II -> Article Image url will attached with content. 
 // Id review reject, then all the corresponding image will be deleted from storage.
 
 // Upload Article Content
