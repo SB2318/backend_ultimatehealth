088975a1e882cd030e2e1f8d7302dbf45d2e2e6d
SB2318 <sb2318@it.jgec.ac.in>
2025-01-22 10:41:46 +0530

    --admin--work--start

---
+++ controllers/admin/adminAuthController.js
@@ -0,0 +1,6 @@
+// moderator registration
+// moderator verification
+// moderator and admin login
+// moderator and admin logout
+// moderator account deletion
+
---
+++ controllers/admin/adminDashboardController.js
@@ -0,0 +1,4 @@
+// article review section
+// article edit request section
+// article and comment report section
+// moderator contribution section
