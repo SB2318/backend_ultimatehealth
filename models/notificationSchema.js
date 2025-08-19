9125b97db6d3573de4753cd6e1b6d9dc92447e38
SB2318 <sb2318@it.jgec.ac.in>
2025-08-19 11:01:51 +0530

    --notification--url--work--start

--- models/notificationSchema.js
+++ models/notificationSchema.js
@@ -1,15 +1,29 @@
 const mongoose = require('mongoose');
 
-const notifcationSchema = new mongoose.Schema({
-    userId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User',
-        required:true
+const notificationSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+       // ref: 'User', 
+        required: true
     },
-    role:{
-      type : Number,
-      required:true,
-      default: 2 // 1-> admin or reviewr , 2-> User
+    role: {
+        type: Number,
+        required: true,
+        default: 2 // 1 -> admin/reviewer, 2 -> user
+    },
+    type: {
+        type: String,
+        required: true,
+        enum: [
+            'articleReview',
+            'mention',
+            'repost',
+            'userFollow',
+            'commentLike',
+            'comment',
+            'post',
+            'postLike'
+        ]
     },
     title: {
         type: String,
@@ -19,8 +33,29 @@ const notifcationSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    read: { type: Boolean, default: false },
-    timestamp: { type: Date, default: Date.now },
-})
+    data: {
+        articleId: { type: mongoose.Schema.Types.ObjectId, ref: 'Article' },
+        podcastId: { type: mongoose.Schema.Types.ObjectId, ref: 'Podcast' },
+        authorId: { type: mongoose.Schema.Types.ObjectId, default: null },
+        recordId: { type: String, required: true },
+        // 1-> article, 2->podcast, 3-> comment, 4-> improvement
+        postType: { type: Number, default: 1 }, 
+        mentionedUsers: [{ type: mongoose.Schema.Types.ObjectId, default: null }],
+        commentId: { type: mongoose.Schema.Types.ObjectId, default: null },
+        senderId: { type: mongoose.Schema.Types.ObjectId, default: null },
+        senderRole: { type: Number },
+        receiverId: { type: mongoose.Schema.Types.ObjectId, default: null },
+        receiverRole: { type: Number },
+        userHandle: { type: String }
+    },
+    read: {
+        type: Boolean,
+        default: false
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
 
-module.exports = mongoose.model('Notification',  notifcationSchema);
+module.exports = mongoose.model('Notification', notificationSchema);
